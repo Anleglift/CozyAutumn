005bb8551c710b78854f13666a508502ed7f9ac0
Anleglift <126957403+Anleglift@users.noreply.github.com>
2023-09-30 01:16:28 +0300

    experimented with movement

--- Cozy Autumn/Assets/Mihnea+Theo movement/ThirdPersonMovement.cs
+++ Cozy Autumn/Assets/Mihnea+Theo movement/ThirdPersonMovement.cs
@@ -10,6 +10,11 @@ public class ThirdPersonMovement : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
     public Animator animator;
+    public float groundRayDistance = 1.0f; // Adjust this value based on your character's size
+    public float fallSpeed = 10.0f;
+    public float walkfallspeed = 11.5f;// Adjust the fall speed as needed
+    public GameObject GroundCheck;
+    public bool isGrounded; // Flag to store whether the player is on the ground
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -17,6 +22,10 @@ public class ThirdPersonMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.Raycast(GroundCheck.transform.position, Vector3.down, groundRayDistance);
+
+        // If not grounded, move the player downward
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -28,11 +37,20 @@ public class ThirdPersonMovement : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
-
+            if (!isGrounded)
+            {
+                Vector3 fallVector = Vector3.down * walkfallspeed * Time.deltaTime;
+                transform.position += fallVector;
+            }
             animator.SetBool("Run", true);
         }
         else
         {
+            if (!isGrounded)
+            {
+                Vector3 fallVector = Vector3.down * fallSpeed * Time.deltaTime;
+                transform.position += fallVector;
+            }
             animator.SetBool("Run", false);
         }
     }
