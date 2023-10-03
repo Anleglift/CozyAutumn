0b203421b505ce3d67bdb88bcba9634ad0acdea6
Vicu <dinuvictor42@gmail.com>
2023-10-03 20:12:42 +0300

    zona
    are o zona in care poti sa i dai pickup sa nu i dai pickupo din pzida masii

--- Cozy Autumn/Assets/Dinu + Cosmin/pickupScript.cs
+++ Cozy Autumn/Assets/Dinu + Cosmin/pickupScript.cs
@@ -5,48 +5,78 @@ using UnityEngine.AI;
 
 public class pickupScript : MonoBehaviour
 {
+    [Header("setari")]
+    public float throwForce;
+    public float delayTime = 1f;
+
+
+    [Header("celelalte")]
     public GameObject fox;
     public GameObject character;
-    private bool isHeld = false;
     public NavMeshAgent navObstacle;
     public Transform handPos;
     public GameObject childObj;
     public CapsuleCollider colider;
 
-    void getFox()
-    {
-        Transform getHand = character.transform.Find("RightHand");
-        Debug.Log(getHand.position);
-    }
+    private bool isHeld = false;
+    private bool isHeldCol = false;
+    private Rigidbody rb;
+
     void Start()
     {
-
+        rb = fox.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
     }
-
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if (!isHeld)
+            if (!isHeld && isHeldCol == true)
             {
                 isHeld = true;
                 fox.transform.position = character.transform.Find("RightHand").position;
                 navObstacle.enabled = false;
                 childObj.transform.SetParent(handPos);
                 colider.enabled = false;
+                rb.isKinematic = true;
             }
             else
             {
                 isHeld = false;
                 navObstacle.enabled = true;
                 childObj.transform.SetParent(null);
+                rb.isKinematic = false;
             }
-
-            
         }
-
-
-
+        if (isHeld && Input.GetMouseButtonDown(0))
+        {
+            isHeld = false;
+            navObstacle.enabled = true;
+            childObj.transform.SetParent(null);
+            colider.enabled = true;
+            rb.isKinematic = false;
+            Vector3 throwDirection = character.transform.forward;
+            rb.velocity = throwDirection * throwForce;
+            Invoke("Delay", delayTime);
+        }
     }
+    void Delay()
+    {
+        rb.isKinematic = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "fox")
+        {
+            isHeldCol = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "fox")
+        {
+            isHeldCol = false;
+        }
+    }
+
 }
