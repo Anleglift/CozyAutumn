8d28373bb9779a6e15967fa12ca4570ab705626b
Vicu <dinuvictor42@gmail.com>
2023-10-03 19:45:30 +0300

    zona
    are o zona in care poti sa i dai pickup sa nu i dai pickupo din pzida masii

--- Cozy Autumn/Assets/Dinu + Cosmin/pickupScript.cs
+++ Cozy Autumn/Assets/Dinu + Cosmin/pickupScript.cs
@@ -1,23 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class pickupScript : MonoBehaviour
 {
+    [Header("setari")]
+    public float delayTime;
+    public float throwForce;
+    public float throwHeight;
+
+
+    [Header("alelalyte")]
     public GameObject fox;
     public GameObject character;
-    private bool isHeld = false;
     public NavMeshAgent navObstacle;
     public Transform handPos;
     public GameObject childObj;
     public CapsuleCollider colider;
+<<<<<<< Updated upstream
 
     void getFox()
     {
         Transform getHand = character.transform.Find("RightHand");
         Debug.Log(getHand.position);
     }
+=======
+    
+    private Rigidbody rb;
+    private bool isHeld = false;
+
+    [Header("MUIEEE BAA")]
+    private bool pula = false;
+>>>>>>> Stashed changes
     void Start()
     {
 
@@ -28,25 +44,57 @@ public class pickupScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if (!isHeld)
+            if (!isHeld && pula == true)
             {
                 isHeld = true;
                 fox.transform.position = character.transform.Find("RightHand").position;
                 navObstacle.enabled = false;
                 childObj.transform.SetParent(handPos);
+<<<<<<< Updated upstream
                 colider.enabled = false;
+=======
+                rb.isKinematic = true;
+>>>>>>> Stashed changes
             }
             else
             {
                 isHeld = false;
                 navObstacle.enabled = true;
                 childObj.transform.SetParent(null);
+<<<<<<< Updated upstream
+=======
+                rb.isKinematic = false;
+>>>>>>> Stashed changes
             }
 
             
         }
 
+<<<<<<< Updated upstream
 
 
+=======
+        if (isHeld && Input.GetMouseButtonDown(0))
+        {
+            isHeld = false;
+            navObstacle.enabled = true;
+            childObj.transform.SetParent(null);
+            rb.isKinematic = false;
+            Vector3 throwDirection = character.transform.forward + Vector3.up * throwHeight;
+            rb.velocity = throwDirection * throwForce;
+            Invoke("Delay", delayTime);
+        }
+>>>>>>> Stashed changes
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "fox")
+            pula = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "fox")
+            pula = false;
     }
 }
