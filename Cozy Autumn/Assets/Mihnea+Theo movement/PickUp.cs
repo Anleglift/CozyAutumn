98c90bd6ed680b3525e7cef7e4a82df444285d4c
Theo20069 <theodor06cismaru@gmail.com>
2023-10-05 15:29:57 +0300

    PickUp finished

--- Cozy Autumn/Assets/Mihnea+Theo movement/PickUp.cs
+++ Cozy Autumn/Assets/Mihnea+Theo movement/PickUp.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public GameObject FoxOnPlayer;
-    public GameObject FoxOnGround; 
-    public float waitTime = 2.0f;
-    public float timer = 0.0f;
+    public GameObject FoxOnGround;
+    public GameObject Player;
+    public GameObject Fox;
+    public float waitTime = 1.0f;
+    public float timer = 0f;
     public bool PickedUp = false;
     public bool Ok = false;
+    public bool interact = false;
     void Start()
     {
         FoxOnPlayer.SetActive(false);
         FoxOnGround.SetActive(true);
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        interact = true;
     }
     void Update()
     {
@@ -23,25 +30,27 @@ public class NewBehaviourScript : MonoBehaviour
             timer = timer - waitTime;
             Ok = true;
         }
-    }
-    private void OnTriggerStay(Collider other)
-    {
         if (Input.GetKey(KeyCode.E)&& Ok == true)
         {
             Ok = false;
-            if (PickedUp == false)
+            if (PickedUp == false &&  interact == true )
             {
                 FoxOnGround.SetActive(false);
                 FoxOnPlayer.SetActive(true);
                 PickedUp = true;
             }
             else
-            {
-                FoxOnGround.SetActive(true);
-                FoxOnPlayer.SetActive(false);
-                PickedUp = false;
-            }
+                if(PickedUp == true)
+                {
+                    Fox.SetActive(false);
+                    Fox.transform.position = Player.transform.position + new Vector3(0.5f, 0f, 0.5f);
+                    Fox.SetActive(true);
+                    FoxOnGround.SetActive(true);
+                    FoxOnPlayer.SetActive(false);
+                    PickedUp = false;
+                }
         }
+        interact = false;
     }
 
 }
